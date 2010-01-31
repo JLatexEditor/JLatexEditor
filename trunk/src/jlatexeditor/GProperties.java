3f2c77a322728611401f3e6df94267cd5cdcde4b
stefan <stefan@bc43685d-03b3-4b44-a042-c3b2e3d7229b>
2010-01-31 08:35:44 +0000

    added BetterProperties2 and first test to write the global config file

--- trunk/src/jlatexeditor/GProperties.java
+++ trunk/src/jlatexeditor/GProperties.java
@@ -1,7 +1,13 @@
 package jlatexeditor;
 
+import de.endrullis.utils.BetterProperties2;
+import de.endrullis.utils.BetterProperties2.Comment;
+import de.endrullis.utils.BetterProperties2.Def;
+import de.endrullis.utils.BetterProperties2.Range;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,15 +15,23 @@ import java.util.HashMap;
  * Global properties for the editor.
  */
 public class GProperties {
+	public static final File CONFIG_FILE = new File(System.getProperty("user.home") + "/.jlatexeditor/global.properties");
   public static HashMap<String,Object> TEXT_ANTIALIAS_MAP = new HashMap<String, Object>();
   public static String[] TEXT_ANTIALIAS_KEYS;
 
   private static ArrayList<String> MONOSPACE_FONTS = new ArrayList<String>();
 
+	private static BetterProperties2 properties = new BetterProperties2();
+	
   // properties
   private static Font editorFont = new Font("MonoSpaced", 0, 13);
   private static Object textAntiAliasing = RenderingHints.VALUE_TEXT_ANTIALIAS_OFF;
 
+	public static final Range INT     = BetterProperties2.INT;
+	public static final Range DOUBLE  = BetterProperties2.DOUBLE;
+	public static final Range BOOLEAN = BetterProperties2.BOOLEAN;
+	public static final Range STRING  = BetterProperties2.STRING;
+	
   static {
     // text anti-aliasing
     TEXT_ANTIALIAS_KEYS = new String[] {"On", "Off", "GASP", "LCD HBGR", "LCD HRGB", "LCD VBGR", "LCD VRGB"};
@@ -45,7 +59,37 @@ public class GProperties {
         MONOSPACE_FONTS.add(fontName);
       }
     }
+
+	  // set default for the properties file
+	  properties.addEntry(new Comment(" Font settings"));
+	  properties.addEntry(new Def("editor.font.name", STRING, "MonoSpaced"));
+	  properties.addEntry(new Def("editor.font.size", INT, "13"));
+	  properties.addEntry(new Def("xwinfo", STRING, null, "xwinfo/xwinfo"));
   }
+
+	public static void load() {
+		if (CONFIG_FILE.exists()) {
+			try {
+				properties.load(new FileReader(CONFIG_FILE));
+			} catch (IOException e) {
+				properties.loadDefaults();
+				e.printStackTrace();
+			}
+		} else {
+			properties.loadDefaults();
+		}
+	}
+
+	public static void save() {
+		CONFIG_FILE.getParentFile().mkdirs();
+		try {
+			properties.store(new FileOutputStream(CONFIG_FILE),
+					" JLatexEditor properties - use control+space to get completion for values\n"+
+					" Default values are commented out\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 
   public static ArrayList<String> getMonospaceFonts() {
