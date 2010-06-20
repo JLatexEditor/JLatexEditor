7a069bb86381b746e82feae3aadc8c030fd3d2d6
Stefan Endrullis <stefan@endrullis.de>
2010-06-20 10:02:29 +0000

    added an exec method allowing us to execute a given commandline string (bash-like syntax with quotes)

--- src/util/ProcessUtil.java
+++ src/util/ProcessUtil.java
@@ -1,8 +1,11 @@
 package util;
 
+import de.endrullis.utils.StringUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 public class ProcessUtil {
@@ -21,4 +24,24 @@ public class ProcessUtil {
 
     return process;
   }
+
+	public static Process exec(String command, File dir) throws IOException {
+	  Process process;
+
+	  ArrayList<String> env = new ArrayList<String>();
+	  for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+	    if (entry.getKey().equalsIgnoreCase("PWD")) continue;
+	    env.add(entry.getKey() + "=" + entry.getValue());
+	  }
+	  String[] envArray = new String[env.size()];
+	  env.toArray(envArray);
+
+		ArrayList<String> list = StringUtils.tokenize(command);
+		String[] array = new String[list.size()];
+		list.toArray(array);
+
+	  process = Runtime.getRuntime().exec(array, envArray, dir);
+
+	  return process;
+	}
 }
