4dc5f2cd6d6805d561d08ae45ffabd87ccc17c25
Stefan Endrullis <stefan@endrullis.de>
2010-09-16 08:49:25 +0000

    log exceptions to error.log

--- startDebug.sh
+++ startDebug.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 svn up
 ant compile
-java -Xprof -cp build/classes/ jlatexeditor.JLatexEditorJFrame "$@" > debug.log &
+java -Xprof -cp build/classes/ jlatexeditor.JLatexEditorJFrame "$@" > debug.log 2> error.log &
