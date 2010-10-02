ca1fc592eb72dd65ab99932472aec70e8dae04a9
stefan <stefan@bc43685d-03b3-4b44-a042-c3b2e3d7229b>
2010-10-02 13:13:14 +0000

    released version 0.1.40

---
+++ tags/0.1.40/src/jlatexeditor/gui/SymbolsPanel.java
@@ -0,0 +1,113 @@
+package jlatexeditor.gui;
+
+import jlatexeditor.JLatexEditorJFrame;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import sce.component.ImageButton;
+import sce.component.SCECaret;
+import sce.component.SCEDocument;
+import sce.component.SCEPane;
+import util.gui.FlowingLayout;
+import util.gui.SCETabbedPaneUI;
+
+import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class SymbolsPanel extends JScrollPane {
+  private JLatexEditorJFrame jLatexEditor;
+
+  private JPanel panel = new JPanel();
+
+  public SymbolsPanel(JLatexEditorJFrame jLatexEditor) {
+    this.jLatexEditor = jLatexEditor;
+
+    panel.setLayout(new FlowingLayout());
+    panel.setBackground(Color.WHITE);
+    panel.setOpaque(true);
+    setViewportView(panel);
+    getVerticalScrollBar().setUnitIncrement(30);
+
+    read(panel, "Relation", "relation");
+    read(panel, "Operators", "operators");
+    read(panel, "Arrows", "arrows");
+    read(panel, "Miscellaneous Math", "misc-math");
+    read(panel, "Miscellaneous Text", "misc-text");
+    read(panel, "Delimiter", "delimiters");
+    read(panel, "Greek", "greek");
+    read(panel, "Special Characters", "special");
+    read(panel, "Cyrillic Characters", "cyrillic");
+  }
+
+  public void doLayout() {
+    panel.setMaximumSize(new Dimension(getViewport().getWidth(), 20000));
+    panel.revalidate();
+    super.doLayout();
+  }
+
+  private void read(JPanel panel, String title, String collection) {
+    JLabel label = new JLabel(title);
+    label.setPreferredSize(new Dimension(160, 30));
+    label.setBackground(SCETabbedPaneUI.BLUE);
+    label.setOpaque(true);
+    label.setBorder(BorderFactory.createEtchedBorder());
+    panel.add(new FlowingLayout.NewLine());
+    panel.add(label);
+    panel.add(new FlowingLayout.NewLine());
+
+    String fileName = "/images/symbols/" + collection + ".xml";
+
+    try {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      Document doc = db.parse(getClass().getResourceAsStream(fileName));
+      doc.getDocumentElement().normalize();
+
+      NodeList commands = doc.getElementsByTagName("commandDefinition");
+      for (int imageNr = 0; imageNr < commands.getLength(); imageNr++) {
+        Node command = commands.item(imageNr);
+        NodeList children = command.getChildNodes();
+        String latexCommand = null;
+        for (int childNr = 0; childNr < children.getLength(); childNr++) {
+          Node child = children.item(childNr);
+          if (child.getNodeName().equals("latexCommand")) latexCommand = child.getTextContent();
+        }
+
+        ImageIcon icon = new ImageIcon(getClass().getResource("/images/symbols/" + collection + "/img" + toString(imageNr + 1) + collection + ".png"));
+        ImageButton button = new ImageButton(icon, icon, icon);
+        panel.add(button);
+        button.addActionListener(new SymbolHandler(latexCommand));
+      }
+    } catch (Exception ignored) {
+      ignored.printStackTrace();
+    }
+  }
+
+  private String toString(int imageNr) {
+    String s = imageNr + "";
+    while (s.length() < 3) s = "0" + s;
+    return s;
+  }
+
+  private class SymbolHandler implements ActionListener {
+    private String command;
+
+    private SymbolHandler(String command) {
+      this.command = command;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+      SCEPane pane = jLatexEditor.getActiveEditor().getTextPane();
+      SCEDocument document = pane.getDocument();
+      pane.clearSelection();
+      SCECaret caret = pane.getCaret();
+      document.insert(command, caret.getRow(), caret.getColumn());
+    }
+  }
+}
---
+++ tags/0.1.40/src/sce/component/SCEDocumentRow.java
@@ -0,0 +1,126 @@
+/**
+ * @author Jörg Endrullis
+ */
+
+package sce.component;
+
+import sce.syntaxhighlighting.ParserStateStack;
+
+public class SCEDocumentRow {
+  // the characters and length of the row
+  public SCEDocumentChar chars[] = new SCEDocumentChar[0];
+  public int length = 0;
+  // the row_nr (position in the document)
+  public int row_nr = 0;
+
+  // the tokens (result of lexer parsing)
+  public ParserStateStack parserStateStack = null;
+  // was the line modified (since last parsing)
+  public boolean modified = true;
+
+  /**
+   * Allocate more characters for a row.
+   *
+   * @param count the number of characters
+   */
+  public void increaseMaxCharacters(int count) {
+    SCEDocumentChar new_chars[] = new SCEDocumentChar[length + count];
+    System.arraycopy(chars, 0, new_chars, 0, chars.length);
+    for (int column_nr = chars.length; column_nr < new_chars.length; column_nr++) {
+      new_chars[column_nr] = new SCEDocumentChar();
+      new_chars[column_nr].column_nr = column_nr;
+      new_chars[column_nr].row = this;
+    }
+    chars = new_chars;
+  }
+
+  /**
+   * Moves the characters behind src position to dest position.
+   *
+   * @param src  the source offset
+   * @param dest the destination offset
+   */
+  public void moveCharacters(int src, int dest) {
+    int move_length = length - src;
+
+    System.arraycopy(chars, src, chars, dest, move_length);
+    if (src < dest) {
+      for (int column_nr = src; column_nr < dest; column_nr++) {
+        chars[column_nr] = new SCEDocumentChar();
+        chars[column_nr].row = this;
+      }
+      for (int column_nr = src; column_nr < dest + move_length; column_nr++) chars[column_nr].column_nr = column_nr;
+    } else {
+      for (int column_nr = dest + move_length; column_nr < src + move_length; column_nr++) {
+        chars[column_nr] = new SCEDocumentChar();
+        chars[column_nr].row = this;
+      }
+      for (int column_nr = dest; column_nr < src + move_length; column_nr++) chars[column_nr].column_nr = column_nr;
+    }
+
+    length = dest + move_length;
+  }
+
+  /**
+   * Sets some characters within the row.
+   *
+   * @param text        the array to take from
+   * @param src         the offset within the array
+   * @param move_length the number of chars to copy
+   * @param dest        the column number (where to put the chars)
+   */
+  public void setCharacters(SCEDocumentChar text[], int src, int move_length, int dest) {
+    for (int char_nr = 0; char_nr < move_length; char_nr++) {
+      chars[dest + char_nr] = text[src + char_nr];
+      chars[dest + char_nr].row = this;
+      chars[dest + char_nr].column_nr = dest + char_nr;
+    }
+  }
+
+  /**
+   * Sets some characters within the row.
+   *
+   * @param charText    the array to take from
+   * @param src         the offset within the array
+   * @param move_length the number of chars to copy
+   * @param dest        the column number (where to put the chars)
+   */
+  public void setCharacters(char charText[], int src, int move_length, int dest) {
+    for (int char_nr = 0; char_nr < move_length; char_nr++) {
+      chars[dest + char_nr].character = charText[src + char_nr];
+      chars[dest + char_nr].style = 0;
+    }
+  }
+
+  /**
+   * Converts the row into a char array.
+   *
+   * @return char array
+   */
+  public char[] toCharArray() {
+    return toCharArray(0, length);
+  }
+
+  public char[] toCharArray(int col_start, int col_end) {
+    if (col_end <= col_start) return new char[0];
+
+    char textChars[] = new char[col_end - col_start];
+    for (int column_nr = col_start; column_nr < col_end; column_nr++) {
+      textChars[column_nr - col_start] = chars[column_nr].character;
+    }
+    return textChars;
+  }
+
+  /**
+   * Converts the row into a string.
+   *
+   * @return the string
+   */
+  public String toString() {
+    return new String(toCharArray());
+  }
+
+  public String toString(int col_start, int col_end) {
+    return new String(toCharArray(col_start, col_end));
+  }
+}
