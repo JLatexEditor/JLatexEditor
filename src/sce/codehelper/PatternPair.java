36c9267ee696d1da3497b9d5461882ff3fe0e428
Stefan Endrullis <stefan@endrullis.de>
2011-06-29 17:55:54 +0000

    fixed bug that led to an exception when jump to was applied outside of the current line

--- src/sce/codehelper/PatternPair.java
+++ src/sce/codehelper/PatternPair.java
@@ -54,6 +54,8 @@ public class PatternPair {
 	 * @return list of groups as WordWithPos if the pattern could be applied or null if it could not match
 	 */
   public List<WordWithPos> find(String rowString, int row, int column) {
+	  if (rowString.length() <= column) return null;
+
     Matcher leftMatcher = leftPattern.matcher(rowString.substring(0, column));
     Matcher rightMatcher = rightPattern.matcher(rowString.substring(column, rowString.length()));
 
