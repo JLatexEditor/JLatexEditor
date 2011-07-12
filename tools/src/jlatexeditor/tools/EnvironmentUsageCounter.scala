537a079b520e07a29a098ce7b1188f11848e0d72
Stefan Endrullis <stefan@endrullis.de>
2011-07-12 20:16:47 +0000

    added EnvironmentUsageCounter

---
+++ tools/src/jlatexeditor/tools/EnvironmentUsageCounter.scala
@@ -0,0 +1,13 @@
+package jlatexeditor.tools
+
+import scala.xml._
+
+/**
+ * Determine how often an environment is used via Google Code Search.
+ *
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+object EnvironmentUsageCounter extends UsageCounter("environmentUsageCounts.properties") {
+	val names = (XML.loadFile("packages.xml") \ "package" \ "environment").map(node => (node \ "@name").text).sortBy(x => x).distinct
+	def getCode(name: String) = "\\\\begin\\{" + name + "\\}"
+}
