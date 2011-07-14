19a3320dc35758c5723bdde3335e6d4c03affd26
stefan <stefan@bc43685d-03b3-4b44-a042-c3b2e3d7229b>
2011-07-14 06:23:49 +0000

    added some util classes

---
+++ trunk/src/de/endrullis/utils/FilterIterable.java
@@ -0,0 +1,23 @@
+package de.endrullis.utils;
+
+import util.Function1;
+
+/**
+ * Filtered iterable.
+ *
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+public class FilterIterable<T> extends ExtIterable<T> {
+	private ExtIterable<T> iterable;
+	private Function1<T, Boolean> filterFunc;
+
+	public FilterIterable(ExtIterable<T> iterable, Function1<T, Boolean> filterFunc) {
+		this.iterable = iterable;
+		this.filterFunc = filterFunc;
+	}
+
+	@Override
+	public ExtIterator<T> iterator() {
+		return new FilterIterator(iterable.iterator(), filterFunc);
+	}
+}
