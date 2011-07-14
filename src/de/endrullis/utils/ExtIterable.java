3344605829ba46ebe7938089877851e4ec33e5c7
Stefan Endrullis <stefan@endrullis.de>
2011-07-14 06:23:49 +0000

    added some util classes

--- src/de/endrullis/utils/ExtIterable.java
+++ src/de/endrullis/utils/ExtIterable.java
@@ -1,5 +1,6 @@
 package de.endrullis.utils;
 
+import com.sun.org.apache.xpath.internal.functions.Function;
 import util.Function1;
 
 import java.util.ArrayList;
@@ -23,16 +24,15 @@ public abstract class ExtIterable<T> implements Iterable<T> {
 	}
 
 	public ArrayList<T> toList(int maxElements) {
-		Iterator<T> iter = iterator();
-		ArrayList<T> list = new ArrayList<T>();
-		for (int i=0; i<maxElements && iter.hasNext(); i++) {
-			list.add(iter.next());
-		}
-		return list;
+		return CollectionUtils.take(this, maxElements);
 	}
 
 	public <T2> ExtIterable<T2> map(Function1<T, T2> mapFunc) {
 		return new MapIterable<T, T2>(this, mapFunc);
+	}
+
+	public ExtIterable<T> filter(Function1<T, Boolean> filterFunc) {
+		return new FilterIterable<T>(this, filterFunc);
 	}
 
 	@Override
---
+++ src/de/endrullis/utils/FilterIterator.java
@@ -0,0 +1,39 @@
+package de.endrullis.utils;
+
+import util.Function1;
+
+/**
+ * Filtered iterator.
+ *
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+public class FilterIterator<T> extends ExtIterator<T> {
+	private ExtIterator<T> iterator;
+	private Function1<T, Boolean> filterFunc;
+	private T next;
+
+	public FilterIterator(ExtIterator<T> iterator, Function1<T, Boolean> filterFunc) {
+		this.iterator = iterator;
+		this.filterFunc = filterFunc;
+	}
+
+	@Override
+	public boolean hasNext() {
+		while (next == null && iterator.hasNext()) {
+			next = iterator.next();
+			if (!filterFunc.apply(next)) {
+				next = null;
+			}
+		}
+		return next != null;
+	}
+
+	@Override
+	public T next() {
+		if (hasNext()) {
+			return next;
+		} else {
+			return null;
+		}
+	}
+}
